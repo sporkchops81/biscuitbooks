6faa3eeb430518d3466d25dbcae33aa6c9c14ce6
Will Cunningham <wrcunningham@gmail.com>
2016-02-09 23:06:38 -0600

    added stubs

--- accounting.py
+++ accounting.py
@@ -1,9 +1,28 @@
 #!/bin/python
 
+import sys
+import sqlite
+
 def main():
     """Run the accounting MVC"""
-    pass()
+    pass
 
-if __name__ == '__main__'
+def add_debit(db, entry):
+    """Add entry to debit book"""
+    pass
+
+def add_credit(db, entry):
+    """Add entry to credit book"""
+    pass
+
+def del_debit(db, entry):
+    """Delete an entry from debit book"""
+    pass
+
+def del_credit(db, entry):
+    """Delete an entry from the credit book"""
+    pass
+
+if __name__ == '__main__':
     status = main()
     sys.exit(status)
